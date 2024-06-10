d512ba5d7f9cb7fc42974a2889a232c593f45b07
Ananbaludsamya <ananbaludsamy5670@gmail.com>
2024-06-10 18:21:37 +0700

    first commit

---
+++ components/sidebar.php
@@ -0,0 +1,153 @@
+<?php $page = substr($_SERVER['SCRIPT_NAME'], strripos($_SERVER['SCRIPT_NAME'], "/") + 1); ?>
+<!-- Main Sidebar Container -->
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars "></i></a>
+        </li>
+    </ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt fa-spin"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+                <i class="fas fa-th-large"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
+<!-- /.navbar -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+        <img src="dist/img/temple.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">ວິທະຍາໄລ </span> <!--ເສຕຖາທິຣາຊ໌ -->
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div </div>
+            </div>
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link <?= $page == 'index.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-home"></i>
+                            <p>
+                                ໜ້າຫຼັກ
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-header">ມະຫາວິທະຍາໄລ</li>
+                    <li class="nav-item <?= $page == 'new_register.php' || $page == 'old_register.php' ? 'menu-open' : '' ?>">
+                        <a href="new_register.php" class="nav-link <?= $page == 'new_register.php' || $page == 'old_register.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-retweet"></i>
+                            <p>
+                                ລົງທະບຽນ
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="new_register.php" class="nav-link <?= $page == 'new_register.php' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        ນັກຮຽນໃຫມ່
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="old_register.php" class="nav-link <?= $page == 'old_register.php' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        ນັກຮຽນເກົ່າ
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'menu-open' : '' ?>">
+                        <a href="rp_Alaryhup.php" class="nav-link <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-retweet"></i>
+                            <p>
+                                ຫ້ອງຮຽນ
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'menu-open' : '' ?>">
+                        <a href="rp_Alaryhup.php" class="nav-link <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-retweet"></i>
+                            <p>
+                                ຄະແນນ
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'menu-open' : '' ?>">
+                        <a href="rp_Alaryhup.php" class="nav-link <?= $page == 'rp_Alaryhup.php' || $page == 'rp_Alaryjay.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-retweet"></i>
+                            <p>
+                                ຈ່າຍຄ່າເທີມ
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item <?= $page == 'i_information_student.php' || $page == 'i_information_staff.php' || $page == 'i_information_subject.php' ? 'menu-open' : '' ?>">
+                        <a href="i_information_student.php" class="nav-link <?= $page == 'i_information_student.php' || $page == 'i_information_staff.php' || $page == 'i_information_subject.php' ? 'active' : '' ?>">
+                            <i class="nav-icon fas fa-retweet"></i>
+                            <p>
+                                ຈັດການຂໍ້ມູນພື້ນຖານ
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="i_information_student.php" class="nav-link <?= $page == 'i_information_student.php' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        ນັກຮຽນ
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="i_information_staff.php" class="nav-link <?= $page == 'i_information_staff.php' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        ພະນັກງານ
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="i_information_subject.php" class="nav-link <?= $page == 'i_information_subject.php' ? 'active' : '' ?>">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        ວີຊາຮຽນ
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+</aside>
---
+++ i_information_subject.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>school-university</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.css">
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">
+</head>
+<style>
+  @media (max-width: 767.98px) {
+    .small-box p {
+      font-size: 16px;
+    }
+  }
+</style>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="dist/img/temple.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+    <?php include "components/sidebar.php"; ?>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0" style="font-weight: bold;">ນັກຮຽນໃຫມ່</h1>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+      <!-- Main content -->
+      <section class="content">
+        
+      </section>
+    </div>
+    <!-- /.content-wrapper -->
+    <?php include "components/footer.php"  ?>
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="plugins/jquery/jquery.min"></script>
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="dist/js/adminlte.js"></script>
+  <script src="dist/js/alert.js"></script>
+  <script src="dist/js/pages/dashboard.js"></script>
+  <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
+  <script src="plugins/toastr/toastr.min.js"></script>
+  <script src="plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="plugins/jszip/jszip.min.js"></script>
+  <script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <script src="plugins/chart.js/Chart.min.js"></script>
+  <script src="dist/js/pages/dashboard3.js"></script>
+  <script>
+    $(function() {
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        "buttons": ["excel", "pdf", "print"],
+        "language": {
+          "info": "ສະແດງ _START_ ຫາ _END_ ຂອງ _TOTAL_ ໜ້າ",
+        },
+        "order": [],
+        "columnDefs": [{
+          "targets": 'no-sort',
+          "orderable": false,
+        }],
+      }).buttons().container().appendTo('#example2_wrapper .col-md-6:eq(0)');
+    });
+  </script>
+  <script>
+    /* global Chart:false */
+
+    $(function() {
+      'use strict'
+
+      var ticksStyle = {
+        fontColor: '#495057',
+        fontStyle: 'bold'
+      }
+
+      var mode = 'index'
+      var intersect = true
+
+      var $salesChart = $('#sales-chart')
+      // eslint-disable-next-line no-unused-vars
+      var salesChart = new Chart($salesChart, {
+        type: 'bar',
+        data: {
+          labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+          datasets: [{
+              backgroundColor: '#007bff',
+              borderColor: '#007bff',
+              data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+            },
+            {
+              backgroundColor: '#ced4da',
+              borderColor: '#ced4da',
+              data: [700, 1700, 2700, 2000, 1800, 1500, 2000]
+            }
+          ]
+        },
+        options: {
+          maintainAspectRatio: false,
+          tooltips: {
+            mode: mode,
+            intersect: intersect
+          },
+          hover: {
+            mode: mode,
+            intersect: intersect
+          },
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [{
+              // display: false,
+              gridLines: {
+                display: true,
+                lineWidth: '4px',
+                color: 'rgba(0, 0, 0, .2)',
+                zeroLineColor: 'transparent'
+              },
+              ticks: $.extend({
+                beginAtZero: true,
+                // Include a dollar sign in the ticks
+                callback: function(value) {
+                  if (value >= 500) {
+                    value /= 500
+                    value += 'k'
+                  }
+                  return '$' + value
+                }
+
+              }, ticksStyle)
+            }],
+            xAxes: [{
+              display: true,
+              gridLines: {
+                display: false
+              },
+              ticks: ticksStyle
+            }]
+          }
+        }
+      })
+    })
+
+    // lgtm [js/unused-local-variable]
+  </script>
+
+</body>
+
+</html>
